442577f1ed1cd9ab75d555373f60c12996db623f
Jorge Garcilazo <jorgegarcilazo99@gmail.com>
2026-03-26 12:29:28 -0600

    updated CORS for production url

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://ticket-factory-client.onrender.com'],
     credentials: true
 }));
 app.use(express.urlencoded());
